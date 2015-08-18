0cf3a777440fd918badc53c0fad34cc47759b85d
Ubuntu <ubuntu@ip-10-0-2-125.ap-southeast-1.compute.internal>
2015-08-18 19:18:08 +0000

    fix run script

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
-cp -r ../appd_play_example /vol/appd_play_example
-cp -r /vol/appd_play_example/AppDynamics /vol/AppDynamics
 cd /vol/appd_play_example
 chmod +x ./activator
 sudo kill -9 $(pgrep -f "/vol/appd_play_example")
 rm -f /vol/appd_play_example/RUNNING_PID
-rm -rf appd_play_example-1.0-SNAPSHOT
+rm -rf appd_play_example-avengers
 ./activator clean dist  && unzip target/universal/*.zip
-cd appd_play_example-1.0-SNAPSHOT
+cd appd_play_example-avengers
 nohup sudo ./bin/appd_play_example -J-javaagent:/vol/AppDynamics/javaagent.jar -Djdk.tls.rejectClientInitiatedRenegotiation=true -Djdk.tls.ephemeralDHKeySize=2048 -Duser.dir=/vol/appd_play_example -Dlogger.file=/vol/appd_play_example/conf/logger.xml &
